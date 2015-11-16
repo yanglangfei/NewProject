ec4aaefccff41b567939d135e593705430158d44
langfei yang <185601452@qq.com>
2015-11-16 18:06:51 +0800

    ViewStub

---
+++ app/src/main/java/com/ylf/jucaipen/newproject/MainActivity.java
@@ -0,0 +1,54 @@
+package com.ylf.jucaipen.newproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewStub show;
+    private Button isShow;
+    private  Button isLoad;
+    private  Button isGone;
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        show= (ViewStub) findViewById(R.id.show);
+        isShow= (Button) findViewById(R.id.isShow);
+        isLoad= (Button) findViewById(R.id.isLoad);
+        isGone= (Button) findViewById(R.id.isGone);
+        isLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view=show.inflate();
+            }
+        });
+        isShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.setVisibility(View.VISIBLE);
+            }
+        });
+        isGone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.setVisibility(View.GONE);
+            }
+        });
+    }
+}
